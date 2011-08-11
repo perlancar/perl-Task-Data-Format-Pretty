d235b0ac231944aba33bc6f94bfce1d320e4a166
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-11 12:59:11 +0700

    add ::Perl

---
+++ lib/Task/Data/Format/Pretty.pm
@@ -0,0 +1,27 @@
+package Task::Data::Format::Pretty;
+1;
+# ABSTRACT: Install Data::Format::Pretty and all known formatters
+__END__
+
+=head1 DESCRIPTION
+
+This task will install Data::Format::Pretty and all Data::Format::Pretty::*
+formatter modules.
+
+=pkgroup Included modules
+
+=pkg Data::Format::Pretty
+
+=pkg Data::Format::Pretty::Console
+
+=pkg Data::Format::Pretty::HTML
+
+=pkg Data::Format::Pretty::JSON
+
+=pkg Data::Format::Pretty::Perl
+
+=pkg Data::Format::Pretty::PHP
+
+=pkg Data::Format::Pretty::YAML
+
+=cut
